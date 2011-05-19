69436dae59c75acde336e354a384b24bd8acf848
linb.net <linb.net@67bf7f17-0434-0410-b960-af5b2157b5a8>
2011-05-19 11:33:29 +0000

    ColLayout minor fix

--- trunk/jsLinb4.0/runtime/jsLinb/js/linb-debug.js
+++ trunk/jsLinb4.0/runtime/jsLinb/js/linb-debug.js
@@ -31600,6 +31600,8 @@ Class("linb.UI.ColLayout",["linb.UI","linb.absList"],{
 
             if(prf && prf!=basePrf){
 
+                var flag,items=profile.children;
+
                 // add to collayout, or move to the right container first
                 if(prf.parent!=profile || prf.childrenId != col){
                     this.append(prf, col);
@@ -31607,7 +31609,6 @@ Class("linb.UI.ColLayout",["linb.UI","linb.absList"],{
                 }
                 
                 var node,
-                    flag,
                     tnode=prf.getRootNode();
 
                 // reposition
@@ -31622,12 +31623,28 @@ Class("linb.UI.ColLayout",["linb.UI","linb.absList"],{
                     if(node.prev().isEmpty() || node.prev().id()!=tnode.id){
                         node.addPrev(tnode);
                         flag=1;
+                        
+                        var i1=_.arr.subIndexOf(items,'0',basePrf),
+                            i2=_.arr.subIndexOf(items,'0',prf);
+                        if(i1!=-1){
+                            var item=items[i2];
+                            _.arr.removeFrom(items, i2);
+                            _.arr.insertAny(items, item, i1, true);
+                        }
                     }
                 }else{
                     node=basePrf.getRoot();
                     if(node.next().isEmpty() || node.next().id()!=tnode.id){
                         node.addNext(tnode);
                         flag=1;
+
+                        var i1=_.arr.subIndexOf(items,'0',basePrf),
+                            i2=_.arr.subIndexOf(items,'0',prf);
+                        if(i1!=-1 && i1 > items.length){
+                            var item=items[i2];
+                            _.arr.removeFrom(items, i2);
+                            _.arr.insertAny(items, item, i1+1, true);
+                        }
                     }
                 }
 
@@ -31841,7 +31858,6 @@ Class("linb.UI.ColLayout",["linb.UI","linb.absList"],{
             }
         },
         DataModel:{
-            selectable:true,
             position:'absolute',
             dock:'fill',
             listKey:null,
