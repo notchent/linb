a15592d559f18d45e4cf571690ec530ee87783da
linb.net <linb.net@67bf7f17-0434-0410-b960-af5b2157b5a8>
2011-06-22 02:06:33 +0000

    Fixed a bug in TreeView: multibycheckbox doesn't work

--- trunk/jsLinb4.0/runtime/jsLinb/js/linb-debug.js
+++ trunk/jsLinb4.0/runtime/jsLinb/js/linb-debug.js
@@ -28905,7 +28905,7 @@ Class("linb.UI.TreeView","linb.UI.TreeBar",{
                     return profile.box._onkeydownbar(profile,e,linb.use(src).parent().linbid());
                 }
             },
-            MARK2:{
+            MARK:{
                 onClick:function(profile, e, src){
                    return profile.box._onclickbar(profile,e,linb.use(src).parent().linbid());
                 }
