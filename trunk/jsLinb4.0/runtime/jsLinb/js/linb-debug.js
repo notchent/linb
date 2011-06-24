cbac0ee6f14b7ba6ec57a28ea8f660494cec5ff4
linb.net <linb.net@67bf7f17-0434-0410-b960-af5b2157b5a8>
2011-06-24 07:54:36 +0000

    Basic JMS added: linb.publish/linb.subscribe/linb.unsubscribe/linb.getSubscribers

--- trunk/jsLinb4.0/runtime/jsLinb/js/linb-debug.js
+++ trunk/jsLinb4.0/runtime/jsLinb/js/linb-debug.js
@@ -705,7 +705,9 @@ _.merge(linb,{
         hookKey:{},
         hookKeyUp:{},
         snipScript:{},
-
+        
+        subscribes:{},
+        
         //ghost divs
         ghostDiv:[],
 
@@ -720,6 +722,50 @@ _.merge(linb,{
         //cache [key]=>[event handler] map for UIProfile
         UIKeyMapEvents:{}
     },
+    subscribe:function(topic, subscriber, receiver, asy){
+        if(topic===null||topic===undefined||subscriber===null||subscriber===undefined||typeof receiver!='function')return;
+        var c=linb.$cache.subscribes,i;
+        c[topic]=c[topic]||[];
+        i=_.arr.subIndexOf(c[topic],"id",subscriber);
+        if(i!=-1)_.arr.removeFrom(c[topic],i);
+        return c[topic].push({id:subscriber,receiver:receiver,asy:!!asy});
+    },
+    unsubscribe:function(topic, subscriber){
+        var c=linb.$cache.subscribes,i;
+        if(!subscriber){
+            if(topic===null||topic===undefined)
+                c={};
+            else
+                delete c[topic];
+        }else if(c[topic]){
+            i=_.arr.subIndexOf(c[topic],"id",subscriber);
+            if(i!=-1)_.arr.removeFrom(c[topic],i);
+        }
+    },
+    publish:function(topic, args, scope){
+        var c=linb.$cache.subscribes;
+        if(topic===null||topic===undefined){
+            for(var topic in c){
+                _.arr.each(c[topic],function(o){
+                    if(o.asy)
+                        _.asyRun(o.receiver, 0, args, scope);
+                    else
+                        return _.tryF(o.receiver, args, scope, true);
+                });
+            }
+        }else if(c[topic]){
+            _.arr.each(c[topic],function(o){
+                if(o.asy)
+                    _.asyRun(o.receiver, 0, args, scope);
+                else
+                    return _.tryF(o.receiver, args, scope, true);
+            });
+        }
+    },
+    getSubscribers:function(topic){
+        return (topic===null||topic===undefined)?linb.$cache.subscribes:linb.$cache.subscribes[topic];
+    },
+
     setDateFormat:function(format){linb.$dateFormat=format},
     getDateFormat:function(){return linb.$dateFormat},
 
