8c155da689d2928c7c0d307033197071403cb3b1
linb.net <linb.net@67bf7f17-0434-0410-b960-af5b2157b5a8>
2011-07-01 09:02:12 +0000

    Enhanced linb.UI.setTheme

--- jsLinb3.1/jsLinb/js/UI.js
+++ jsLinb3.1/jsLinb/js/UI.js
@@ -3265,7 +3265,7 @@ Class("linb.UI",  "linb.absObj", {
         getTheme:function(){
             return this.$theme;
         },
-        setTheme:function(key, refresh){
+        setTheme:function(key, refresh, onSucess, onFail){
             key=key||'default';
             var ns=this;
             if(key!=ns.$theme){
@@ -3283,8 +3283,10 @@ Class("linb.UI",  "linb.absObj", {
                     ns.$CSSCACHE={};
                     var count=0;
                     _.asyRun(function(){
-                        if(count>10)
-                            throw new Error('errLoadTheme:'+key);
+                        if(count>10){
+                            if(false!==_.tryF(onFail))
+                                throw new Error('errLoadTheme:'+key);
+                        }
                         count++;
                         var s;
                         try{
@@ -3292,6 +3294,7 @@ Class("linb.UI",  "linb.absObj", {
                         }catch(e){}finally{
                             if(s==key || key=='default'){
                                 linb.UI.getAll().reLayout(true);
+                                _.tryF(onSucess);
                                 count=null;
                             }else
                                 _.asyRun(arguments.callee,200*count);
